c14eefc83c29213ca460bf3a9e0789936671b0ad
viniciuscosta110 <viniciuscosta1900@hotmail.com>
2019-09-11 08:39:51 -0300

    Tindev Web 1.0

---
+++ tindev/App.js
@@ -0,0 +1,26 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React from 'react';
+import {Text} from 'react-native';
+
+import {
+  Header,
+  LearnMoreLinks,
+  Colors,
+  DebugInstructions,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+const App = () => {
+  return (
+    <Text>Hello World</Text>
+  );
+};
+
+export default App;
